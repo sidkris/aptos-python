5f464cc0444b6f309c8b223adcd3f199d5b3e83d
Siddharth Krishnan <sidkrishnan@protonmail.com>
2025-07-31 21:19:08 +0530

    Update transaction.py

--- my-first-transaction/transaction.py
+++ my-first-transaction/transaction.py
@@ -43,5 +43,83 @@ async def main():
     logging.info(f"Bob: {bob_balance} octas")
 
 
+    logging.info("\n\n*** 1. Building the Transaction ***\n")
+
+    entry_function = EntryFunction.natural(
+            "0x1::aptos_account",  # Module address and name
+            "transfer",            # Function name
+            [],                    # Type arguments (empty for this function)
+            [
+                # Function arguments with their serialization type
+                TransactionArgument(bob.address(), Serializer.struct),  # Recipient address
+                TransactionArgument(1000, Serializer.u64),              # Amount to transfer (1000 octas)
+            ],
+        )   
+
+    # get the chain id for the transaction
+    chain_id = await rest_client.chain_id()
+
+    # get the sender's current sequence number
+    account_data = await rest_client.account(alice.address())
+    sequence_number = int(account_data["sequence_number"])
+
+    # create the raw transaction with all the required fields
+    raw_transaction = RawTransaction(
+        sender = alice.address(),                                   # Sender's address
+        sequence_number = sequence_number,                          # Sequence number to prevent a relay attack
+        payload = TransactionPayload(entry_function),               
+        max_gas_amount = 2000,                                      # Max gas units to use
+        gas_unit_price = 100,                                       # Price per gas unit in octas
+        expiration_timestamps_secs = int(time.time()) + 600,        # Expires in 10 mins
+        chain_id = chain_id,                                        # Chain id to ensure correct network
+    )
+
+
+    logging.info("Transaction built successfully")
+    logging.info(f"Sender: {raw_transaction.sender}")                                                   
+    logging.info(f"Sequence Number: {raw_transaction.sequence_number}")               
+    logging.info(f"Max Gas Amount: {raw_transaction.max_gas_amount}")                  
+    logging.info(f"Gas Unit Price: {raw_transaction.gas_unit_price}")
+    logging.info(f"Expiration Timestamp: {time.ctime(raw_transaction.expiration_timestamps_secs)}")
+
+
+    logging.info("\n\n*** 2. Simulating the Transaction ***\n") # Before submitting a transaction, it’s wise to simulate it first to estimate the gas cost. This is like checking shipping costs before sending a package.
+
+    # Create a BCS transaction for simulation
+    # This doesn't actually submit the transaction to the blockchain
+
+    simulation_transaction = await rest_client.create_bcs_transaction(alice, TransactionPayload(entry_function))
+    simulation_result = await rest_client.simulate_transaction(simulation_transaction, alice) # Simulate tranasaction to estimate gas cost and check for errors
+
+    gas_used = int(simulation_result[0]["gas_used"])
+    gas_unit_price = int(simulation_result[0]["gas_unit_price"])
+    success = simulation_result[0]["success"]
+
+    logging.info(f"Estimated gas units: {gas_used}")
+    logging.info(f"Estimated gas cost: {gas_used * gas_unit_price} octas")
+    logging.info(f"Transaction would {'succeed' if success else 'fail'}")
+
+
+    logging.info("\n\n*** 3. Signing the Transaction ***\n")
+
+    signed_transaction = await rest_client.create_bcs_signed_transaction(
+        alice,                                                          # Account with the private key
+        TransactionPayload(entry_function),                             # The payload from our transaction
+        sequence_number = sequence_number,                              # Using the same sequence number as before
+    )
+
+
+    logging.info("Transaction signed successfully.")
+
+
+    logging.info("\n\n*** 4. Submitting the Transaction ***\n")
+    transaction_hash = await rest_client.submit_bcs_transaction(signed_transaction)
+    logging.info(f"Transaction submitted with the hash : {transaction_hash}")
+
+
+
+
+
+
 if __name__ == "__main__":
     asyncio.run(main())
